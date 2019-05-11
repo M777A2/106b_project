dfdf621665b47d5ec40b591161f480cbf421fc9a
m777a2 <zephan.the@berkeley.edu>
2019-05-10 20:34:22 -0700

    distance field working well, optimize time :o

--- DistanceField.py
+++ DistanceField.py
@@ -1,85 +1,120 @@
 import copy
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy.linalg import norm
 from typing import Callable
-
-class Px:
-    def __init__(self, dx: int, dy: int):
-        self.dist = np.array([dx, dy])
-
-    def dist_sq(self):
-        return np.linalg.norm(self.dist)
-
-    def __le__(self, other):
-        assert(isinstance(other, Px))
-        return self.dist_sq() <= other.dist_sq()
-
-    def __lt__(self, other):
-        assert (isinstance(other, Px))
-        return self.dist_sq() < other.dist_sq()
-
-    def __sub__(self, other):
-        assert (isinstance(other, Px))
-        return self.dist - other.dist
 
 
 class Grid:
     def __init__(self, w: int = 100, h: int = 100):
         self.width = w
         self.height = h
-        self.grids = [np.array([[Px(0,0)]*w]*h)]*2
+        self.MAX = w**2 + h**2
+        self.grids = [np.array([[np.zeros(2)]*w]*h)]*2
+        self.grids = [np.array([[np.zeros(2)]*w]*h), np.array([[np.ones(2)*10]*w]*h)]
         self.sdt = None
+
+    def _show_grid(self, show) -> str:
+        if show is None:
+            return ""
+        rep = ""
+        for y in range(self.height):
+            line = ""
+            for x in range(self.width):
+                line += " "*(3-len(str(int(norm(show[x, y]))))) + str(int(norm(show[x, y])))
+            rep += line + "\n"
+        return rep
+
+    def __repr__(self):
+        return self._show_grid(self.sdt)
+
+    def __str__(self):
+        return self.__repr__()
 
     def add_object(self, constraint: Callable[[int, int], bool]):
         for x in range(self.width):
             for y in range(self.height):
                 if constraint(x, y):
-                    self.grids[0][x, y] = Px(0,0)
-                    self.grids[1][x, y] = Px(np.inf, np.inf)
+                    self.grids[0][x, y] = np.array([0, 0])
+                    self.grids[1][x, y] = np.array([self.MAX, self.MAX])
                 else:
-                    self.grids[0][x, y] = Px(np.inf, np.inf)
-                    self.grids[1][x, y] = Px(0,0)
+                    self.grids[0][x, y] = np.array([self.MAX, self.MAX])
+                    self.grids[1][x, y] = np.array([0, 0])
+
+        for grid in range(2):
+            print("grid: {}".format(grid))
+            print(self._show_grid(self.grids[grid]))
 
     def _compare(self, grid, x: int, y: int, offset_x: int, offset_y: int):
-        neighbor =  copy.deepcopy(self.grids[grid][x+offset_x,y+offset_y])
-        neighbor.dist += np.array([offset_x,offset_y])
+        if 0 <= (x+offset_x) < self.width and 0 <= (y+offset_y) < self.height:
+            neighbor = copy.deepcopy(self.grids[grid][x+offset_x, y+offset_y])
+            neighbor += np.array([offset_x, offset_y])
+        else:
+            neighbor = np.array([self.MAX, self.MAX])
 
-        if neighbor < self.grids[grid][x, y]:
-            self.grids[grid][x,y] = neighbor
+        if sum(neighbor**2) < sum(self.grids[grid][x, y]**2):
+            # print("changing value [{}, {}]".format(x, y))
+            # print("grid: {}".format(grid))
+            # print(self._show_grid(self.grids[grid]))
+            self.grids[grid][x, y] = neighbor
 
     def generateSDT(self):
-        y_range = list(range(1, self.height-1))
-        x_range = list(range(1, self.width-1))
-        for i in range(2):
+        y_range = list(range(self.height))
+        y_rev = list(y_range)
+        y_rev.reverse()
+        x_range = list(range(self.width))
+        x_rev = list(x_range)
+        x_rev.reverse()
+
+        for i in range(2):  # compute for each grid separately
             for y in y_range:
-                print("y = {}".format(y))
+                print("y = ", y)
                 for x in x_range:
                     self._compare(i, x, y, -1,  0)
                     self._compare(i, x, y,  0, -1)
                     self._compare(i, x, y, -1, -1)
                     self._compare(i, x, y,  1, -1)
-                x_range.reverse()
-                for x in x_range:
+                for x in x_rev:
                     self._compare(i, x, y,  1,  0)
 
-            y_range.reverse()
-            x_range.reverse()
-            for y in y_range:
-                for x in x_range:
+            for y in y_rev:
+                print("y = ", y)
+                for x in x_rev:
                     self._compare(i, x, y,  1,  0)
                     self._compare(i, x, y,  0,  1)
                     self._compare(i, x, y, -1,  1)
                     self._compare(i, x, y,  1,  1)
-                x_range.reverse()                    
                 for x in x_range:
-                    self._compare(i, x, y,  -1,  0)
-            y_range.reverse()
-            x_range.reverse()
+                    self._compare(i, x, y, -1,  0)
 
-        self.sdt = self.grids[0] - self.grids[1]
+        self.sdt = np.zeros([self.width, self.height])
+        for y in range(self.height):
+            for x in range(self.width):
+                self.sdt[x, y] = norm(self.grids[0][x, y]) - norm(self.grids[1][x, y])
+
+
+    def get(self) -> np.array:
+        return self.sdt
+
 
 if __name__ == "__main__":
-    grid = Grid()
-    grid.add_object(lambda x, y: 30<x<50 and 30<y<50)
+    grid = Grid(60,60)
+    grid.add_object(lambda x, y: 30 < x < 60 and 30 < y < 60)
     grid.generateSDT()
     print("sdt done")
-    print(grid.sdt)
+    print(grid)
+
+    # Import Dataset
+    df = grid.get()
+
+    # Plot
+    plt.figure(figsize=(60, 60), dpi=80)
+    sns.heatmap(df, xticklabels=range(60), yticklabels=range(60), cmap='RdYlGn', center=0,
+                annot=True)
+
+    # Decorations
+    plt.title('Signed Distance Field', fontsize=22)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.show()
---
+++ optimize.py
@@ -0,0 +1,4 @@
+from DistanceField import Grid
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+from
